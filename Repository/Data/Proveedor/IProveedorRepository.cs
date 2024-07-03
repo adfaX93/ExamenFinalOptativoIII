8d9e623d5eb6103174f58e0befe68fbdf21d15bd
Alex Fretes <67652643+adfaX93@users.noreply.github.com>
2024-07-02 21:07:35 -0400

    se crea IProveedorRepository

---
+++ Repository/Data/Proveedor/IProveedorRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Data.Proveedor
+{
+    public interface IProveedorRepository
+    {
+        bool add(ProveedorModel proveedorModel);
+        bool update(ProveedorModel proveedorModel);
+        bool delete(int id);
+        IEnumerable<ProveedorModel> GetAll();
+    }
+}
