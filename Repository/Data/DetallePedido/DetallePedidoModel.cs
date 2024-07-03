27023875ea1b3082bf2a7e2306fef3df47cbd80f
Alex Fretes <67652643+adfaX93@users.noreply.github.com>
2024-07-02 22:40:53 -0400

    se crea DetallePedido

---
+++ Repository/Data/DetallePedido/DetallePedidoModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Data.DetallePedido
+{
+    public class DetallePedidoModel
+    {
+        public int IdDetallePedido { get; set; }
+        public int IdPedido { get; set; }
+        public int IdProducto { get; set; }
+        public int CantidadPedido { get; set; }
+        public decimal Subtotal { get; set; }
+    }
+}
