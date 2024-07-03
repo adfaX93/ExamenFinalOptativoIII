3d9e3e5b9373c4e1b07b830bc30b45b88d8f8356
Alex Fretes <67652643+adfaX93@users.noreply.github.com>
2024-07-02 20:47:15 -0400

    se crea ProveedorModel

---
+++ Repository/Data/Proveedor/ProveedorModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Data.Proveedor
+{
+    public class ProveedorModel
+    {
+        public int IdProveedor { get; set; }
+        public string RazonSocial { get; set; }
+        public string TipoDocumento { get; set; }
+        public string NumeroDocumento { get; set; }
+        public string Direccion { get; set; }
+        public string Mail { get; set; }
+        public string Celular { get; set; }
+        public string Estado { get; set; } 
+    }
+}
