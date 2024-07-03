211543633e013cd1aa048a3b6df35ab650825960
Alex Fretes <67652643+adfaX93@users.noreply.github.com>
2024-07-02 20:05:43 -0400

    se crea la clase ConnectionDB

---
+++ Repository/Data/ConfiguracionesDB/ConnectionDB.cs
@@ -0,0 +1,26 @@
+﻿using Npgsql;
+
+namespace Repository.Data.ConfiguracionesDB
+{
+    public class ConnectionDB
+    {
+        private string conexionString;
+        public ConnectionDB(string conexionString)
+        {
+            this.conexionString = conexionString;
+        }
+        public NpgsqlConnection OpenConnection()
+        {
+            try
+            {
+                var conn = new NpgsqlConnection(conexionString);
+                conn.Open();
+                return conn;
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
