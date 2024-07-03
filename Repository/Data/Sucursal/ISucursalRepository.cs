6e6b42a398fb8780a4fcecc5bca0706f52833ccb
Alex Fretes <67652643+adfaX93@users.noreply.github.com>
2024-07-02 21:51:29 -0400

    se crea ISucursalRepository

---
+++ Repository/Data/Sucursal/ISucursalRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Data.Sucursal
+{
+    public interface ISucursalRepository
+    {
+        bool add(SucursalModel sucursalModel);
+        bool update(SucursalModel sucursalModel);
+        bool delete(int id);
+        IEnumerable<SucursalModel> GetAll();
+    }
+}
