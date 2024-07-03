7ea12b42de205593b31995ffe1312bd5f44d128b
Alex Fretes <67652643+adfaX93@users.noreply.github.com>
2024-07-02 22:13:44 -0400

    se crea ProductoModel

---
+++ Repository/Data/Producto/ProductoModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Data.Producto
+{
+    public class ProductoModel
+    {
+        public int IdProducto { get; set; }
+        public string Descripcion { get; set; }
+        public int CantidadMinima { get; set; }
+        public int CantidadStock { get; set; }
+        public decimal PrecioCompra { get; set; }
+        public decimal PrecioVenta { get; set; }
+        public string Categoria { get; set; }
+        public string Marca { get; set; }
+        public string Estado { get; set; }
+    }
+}
